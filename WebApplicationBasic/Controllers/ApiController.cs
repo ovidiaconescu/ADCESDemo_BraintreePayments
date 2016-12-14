a20dba57b1ae21e094ee7317994beb44cab6f471
demo <demo@ro-3048169.eamobile.ad.ea.com>
2016-12-14 14:05:37 +0200

    ADCES Demo - Braintree Payments Integration

---
+++ WebApplicationBasic/Controllers/ApiController.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Braintree;
+
+namespace WebApplicationBasic.Controllers
+{
+    public class ApiController : Controller
+    {
+        [HttpPost]
+        public IActionResult Hooked()
+        {
+            
+             BraintreeGateway gateway = new BraintreeGateway
+                {
+                    Environment = Braintree.Environment.SANDBOX,
+                    MerchantId = "",
+                    PublicKey = "",
+                    PrivateKey = ""
+                };
+
+
+            var webhook
+                = gateway.WebhookNotification.Parse(Request.Form["bt_signature"], Request.Form["bt_payload"]);
+
+            string message = $"Webhook Received [{webhook.Timestamp.Value}] | Kind: [{webhook.Kind}]";
+
+            Program.messages.Add(message);
+
+
+            return Ok();
+        }
+    }
+}
---
+++ WebApplicationBasic/Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Braintree;
+
+namespace WebApplicationBasic.Controllers
+{
+    public class HomeController : Controller
+    {
+        BraintreeGateway gateway = new BraintreeGateway
+        {
+            Environment = Braintree.Environment.SANDBOX,
+            MerchantId = "...",
+            PublicKey = "...",
+            PrivateKey = "..."
+        };
+
+        public IActionResult Index()
+        {
+            var token = gateway.ClientToken.generate();
+            Console.WriteLine(token);
+            return View(model: token);
+        }
+
+        [HttpPost("Purchase")]
+        public IActionResult Purchase([FromBody] NonceModel model)
+        {
+            var request = new TransactionRequest
+            {
+                Amount = 15.00M,
+                PaymentMethodNonce = model.nonce,
+                Options = new TransactionOptionsRequest
+                {
+                    SubmitForSettlement = true
+                }
+            };
+
+            Result<Transaction> result = gateway.Transaction.Sale(request);
+
+            Transaction transaction = result.Target;
+            TransactionStatus status = transaction.Status;
+            
+            return Json(Url.Action("About", new { message = status.ToString() }));
+
+        }
+
+        public IActionResult About(string message = "")
+        {
+            ViewData["Message"] = message;
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "webhooks";
+            return View(Program.messages);
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+
+    public class NonceModel
+    {
+        public string nonce {get;set;}
+
+    }
+}
